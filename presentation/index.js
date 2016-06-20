a9ea3e14a01dd66ee1573f4c82158452927bcb59
Nicolas Baptiste <nicolas.baptiste@gmail.com>
2016-06-21 00:24:29 +0200

    Tweaks

--- presentation/index.js
+++ presentation/index.js
@@ -48,7 +48,10 @@ const images = {
 preloader(images);
 
 const theme = createTheme({
-  primary: "#ff4081"
+  primary: "#967d4d",
+  secondary: "black",
+  tertiary: "white",
+  quartenary: "white"
 });
 
 export default class Presentation extends React.Component {
@@ -57,23 +60,19 @@ export default class Presentation extends React.Component {
       <Spectacle theme={theme}>
         <Deck transition={["zoom", "slide"]} transitionDuration={500}>
 
-          <Slide transition={["zoom"]} bgColor="primary" bgColor="#5454C7">
-            <Heading size={1} fit caps lineHeight={1}>
-              Flux Architecture
-            </Heading>
-            <small>(&amp; ReactJS)</small>
+          <Slide transition={["zoom"]} bgColor="secondary">
+            <Heading size={1} fit caps lineHeight={1}> Flux Architecture </Heading>
+            <Heading size={2} bgColor="primary" padding=".1em 0 .2em">(&amp; ReactJS)</Heading>
           </Slide>
 
           <Slide>
             <Heading size={1} caps lineHeight={1}>
               Server side MVC
             </Heading>
-            <CodePane
-              textSize="25px"
-              lang="jsx"
-              source={require("raw!../assets/mvc.example")}
-              margin="20px auto"
-            />
+            <CodePane textSize=".75em"
+                      lang="javascript"
+                      source={require("raw!../assets/mvc.example")}
+                      margin="20px auto"/>
           </Slide>
 
           <Slide transition={["slide"]} bgColor="white">
@@ -108,15 +107,19 @@ export default class Presentation extends React.Component {
             <Heading size={2} caps textColor="tertiary">
               Flux
             </Heading>
-            <Image src={images.flux.replace("/", "")} margin="50px auto 0" />
+            <Image src={images.flux.replace("/", "")} margin="50px auto 0"/>
           </Slide>
 
           <Slide>
             <List textColor="tertiary">
-              <ListItem><strong>Actions</strong> – Helper methods that facilitate passing data to the Dispatcher</ListItem>
-              <ListItem><strong>Dispatcher</strong> – Receives actions and broadcasts payloads to registered callbacks</ListItem>
-              <ListItem><strong>Stores</strong> – Containers for application state & logic that have callbacks registered to the dispatcher</ListItem>
-              <ListItem><strong>Controller Views</strong> – React Components that grab the state from Stores and pass it down via props to child components.</ListItem>
+              <ListItem><strong>Actions</strong> – Helper methods that facilitate passing data to the
+                Dispatcher</ListItem>
+              <ListItem><strong>Dispatcher</strong> – Receives actions and broadcasts payloads to registered
+                callbacks</ListItem>
+              <ListItem><strong>Stores</strong> – Containers for application state & logic that have callbacks
+                registered to the dispatcher</ListItem>
+              <ListItem><strong>Controller Views</strong> – React Components that grab the state from Stores and pass it
+                down via props to child components.</ListItem>
             </List>
           </Slide>
 
@@ -124,31 +127,27 @@ export default class Presentation extends React.Component {
             <Heading size={2} caps textColor="primary">
               Flux dataflow
             </Heading>
-            <Image src={images.flux_detail.replace("/", "")} />
+            <Image src={images.flux_detail.replace("/", "")}/>
           </Slide>
 
           <Slide>
-            <Heading size={2} caps textColor="tertiary">
-              The good parts
-            </Heading>
+            <Heading size={2} caps textColor="tertiary"> The good parts </Heading>
             <List textColor="tertiary">
-              <ListItem>Very stricts rules enforced by the Dispatcher</ListItem>
-              <ListItem>Dispatcher = real controller, all states changes have to go through them</ListItem>
-              <ListItem>Stores = Model, not db wrappers but just plain state</ListItem>
+              <ListItem textColor="secondary">Very stricts rules enforced by the Dispatcher</ListItem>
+              <ListItem>Dispatcher = real controller, all states changes go through them</ListItem>
+              <ListItem textColor="secondary">Stores = Model, not db wrappers but just plain state</ListItem>
               <ListItem>Unidirectional dataflow</ListItem>
-              <ListItem>Easier for unit testing</ListItem>
+              <ListItem textColor="secondary">Easier for unit testing</ListItem>
               <ListItem>Great for scaling</ListItem>
             </List>
           </Slide>
 
           <Slide bgColor="secondary">
-            <Heading size={2} caps textColor="primary">
-              The bad parts
-            </Heading>
+            <Heading size={2} caps textColor="tertiary"> The bad parts </Heading>
             <List textColor="tertiary">
-              <ListItem>Lot of implemenations</ListItem>
+              <ListItem textColor="primary">Lot of implemenations</ListItem>
               <ListItem>No middleware for actions (need for extra Actions Creator)</ListItem>
-              <ListItem>View dependency to the store</ListItem>
+              <ListItem textColor="primary">View dependency to the store</ListItem>
               <ListItem>Quite complex maybe not needed for small apps</ListItem>
             </List>
           </Slide>
@@ -162,11 +161,11 @@ export default class Presentation extends React.Component {
 
           <Slide>
             <Heading size={1} caps textColor="tertiary">
-                Redux
+              Redux
             </Heading>
-            <Image src={images.redux.replace("/", "")} width="900px" />
+            <Image src={images.redux.replace("/", "")} width="900px"/>
             <Text>
-            Redux starting course videos : <Link href="https://egghead.io/series/getting-started-with-redux">https://egghead.io/series/getting-started-with-redux</Link>
+              Redux starting course videos : <Link href="https://egghead.io/series/getting-started-with-redux">https://egghead.io/series/getting-started-with-redux</Link>
             </Text>
           </Slide>
 
@@ -176,21 +175,23 @@ export default class Presentation extends React.Component {
             </Heading>
             <List textColor="tertiary">
               <ListItem><strong>Single source of truth:</strong> only one store</ListItem>
-              <ListItem><strong>State is read-only:</strong> the only way to mutate the state is to emit an action</ListItem>
+              <ListItem><strong>State is read-only:</strong> the only way to mutate the state is to emit an
+                action</ListItem>
               <ListItem><strong>Changes</strong> are made with <strong>pure functions</strong></ListItem>
-              <ListItem><strong>Redux does not have the concept of a Dispatcher</strong> pure functions instead of event emitters</ListItem>
+              <ListItem><strong>Redux does not have the concept of a Dispatcher</strong> pure functions instead of event
+                emitters</ListItem>
               <ListItem>Great helpers + tools</ListItem>
-              <ListItem> 
-              <Link href="http://redux.js.org/docs/introduction/Ecosystem.html">
-              http://redux.js.org/docs/introduction/Ecosystem.html
-              </Link></ListItem>
+              <ListItem>
+                <Link href="http://redux.js.org/docs/introduction/Ecosystem.html">
+                  http://redux.js.org/docs/introduction/Ecosystem.html
+                </Link></ListItem>
             </List>
           </Slide>
           <Slide bgColor="secondary">
-            <Heading size={2} caps textColor="tertiary">
+            <Heading size={2} fit caps textColor="tertiary">
               When you code with redux
             </Heading>
-            <Appear><Image src={images.fountain.replace("/", "")} margin="50px auto 0" width="800px" /></Appear>
+            <Appear><Image src={images.fountain.replace("/", "")}  width="100%"/></Appear>
           </Slide>
 
         </Deck>
